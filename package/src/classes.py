6a03764681fa1ce226b71687a73073316f34f991
Daniel Prokopenko <prokopenko2002902@gmail.com>
2024-03-17 16:55:42 +0200

    Comments deleted and PEP 8 changes made

--- package/src/classes.py
+++ package/src/classes.py
@@ -6,6 +6,8 @@ import re
 
 
 class Field:
+    """Represents a field to be filled"""
+
     def __init__(self, value):
         self.value = value
 
@@ -14,6 +16,8 @@ class Field:
 
 
 class Name(Field):
+    """Represents the contact`s name"""
+
     def __init__(self, value):
         if name_is_valid(value):
             self.value = value.title()
@@ -22,6 +26,8 @@ class Name(Field):
 
 
 class Phone(Field):
+    """Represents the contact`s phone number"""
+
     def __init__(self, value):
         if phone_is_valid(value):
             self.value = value
@@ -30,19 +36,18 @@ class Phone(Field):
 
 
 class Email(Field):
+    """Represents the contact`s email"""
+
     def __init__(self, value):
         if email_is_valid(value):
             self.value = value
         else:
             raise ValueError("Invalid email format")
 
-    # @staticmethod
-    # def validate_email(value):
-    #     pattern = r'^[\w\.-]+@[\w\.-]{2,}\.\w{2,}$'
-    #     return re.match(pattern, value) is not None
-
 
 class Birthday(Field):
+    """Represents the contact`s birthday"""
+
     def __init__(self, value: str):
         if date_is_valid(value):
             try:
@@ -54,6 +59,8 @@ class Birthday(Field):
 
 
 class Address(Field):
+    """Represents the contact`s address"""
+
     def __init__(self, street, house_number, city, postal_code=None, country=None):
         self.street = street
         self.house_number = house_number
@@ -71,6 +78,8 @@ class Address(Field):
 
 
 class Note(UserDict):
+    """Represents a note with title, text and tags"""
+
     def __init__(self, title, text, tags):
         super().__init__()
         self.data["title"] = title
@@ -82,6 +91,8 @@ class Note(UserDict):
 
 
 class Record:
+    """Represents a contact in your address book"""
+
     def __init__(self, name):
         self.name = Name(name)
         self.phones = []
@@ -130,31 +141,6 @@ class Record:
         self.addresses.append(Address(street, house_number, city, postal_code, country))
 
     def edit_address(self, street, house_number, city, postal_code=None, country=None):
-        if (
-            self.addresses
-        ):  # варіанти: ?  if self.addresses[0]  # if len(self.addresses) > 0
-            address = self.addresses[0]
-            if street:
-                address.street = street
-            if house_number:
-                address.house_number = house_number
-            if city:
-                address.city = city
-            if postal_code:
-                address.postal_code = postal_code
-            if country:
-                address.country = country
-            return "Address edited."
-        else:
-            return "No address to edit."
-
-    def remove_address(self):
-        self.addresses = []
-
-    def add_address(self, street, house_number, city, postal_code=None, country=None):
-        self.addresses.append(Address(street, house_number, city, postal_code, country))
-
-    def edit_address(self, street, house_number, city, postal_code=None, country=None):
         if self.addresses:
             address = self.addresses[0]
             if street:
@@ -173,15 +159,17 @@ class Record:
 
     def remove_address(self):
         self.addresses = []
-    
+
     def deposit(self, amount):
         self.money += amount
-        
+
     def withdraw(self, amount):
         self.money -= amount
 
 
 class AddressBook(UserDict):
+    """Represents an address book"""
+
     def __init__(self, name="contacts"):
         super().__init__()
         self.name = name
@@ -212,6 +200,7 @@ class AddressBook(UserDict):
             print(f"{name}'s contact wasn't found")
 
     def get_birthdays_per_week(self):
+        """Shows you people who are going tot celebrate their birthday in the next seven days"""
         users = []
         for name, record in self.data.items():
             if record.birthday != None:
@@ -219,6 +208,7 @@ class AddressBook(UserDict):
         return get_birthdays_per_week(users)
 
     def get_birthdays_by_days(self, days):
+        """Shows you people who are going tot celebrate their birthday in the certain amount of days"""
         users = []
         for name, record in self.data.items():
             if record.birthday != None:
@@ -230,14 +220,3 @@ class AddressBook(UserDict):
                     }
                 )
         return get_birthdays_by_days(users, days)
-
-
-class Note(UserDict):
-    def __init__(self, title, text, tags):
-        super().__init__()
-        self.data["title"] = title
-        self.data["text"] = text
-        self.data["tags"] = tags
-
-    def __str__(self):
-        return f"{'=' * 50}\nTitle: {self.data['title']}\nText: {self.data['text']}\nTags: {' '.join(self.data['tags'])}"
